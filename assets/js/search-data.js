1411992fd8bbd94e7a11db5f7c001205407c448f
helemnmmm <126218461+helemnmmm@users.noreply.github.com>
2025-04-27 15:40:13 +0000

    Deploying to gh-pages from @ helemnmmm/helemnmmm.github.io@9a5127e61c2134485697be3c676806ceef9ab18e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -39,7 +39,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "Malin&#39;s academic resume; Last update 2025-04",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
