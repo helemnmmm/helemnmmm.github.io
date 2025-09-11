bb3f3b02049e397d5d578aafd74be9ed3180d69e
helemnmmm <126218461+helemnmmm@users.noreply.github.com>
2025-09-11 14:17:34 +0000

    Deploying to gh-pages from @ helemnmmm/helemnmmm.github.io@ab6048649ea59abd8ac0e2a05940c56bff65cb2e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,13 +16,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/blog/";
           },
-        },{id: "nav-projects",
-          title: "Projects",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/projects/";
-          },
         },{id: "nav-codes",
           title: "Codes",
           description: "",
