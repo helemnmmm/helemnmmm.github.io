14a65f765a19c162acafe4998a78b1c0b0010613
helemnmmm <126218461+helemnmmm@users.noreply.github.com>
2025-11-22 16:29:49 +0000

    Deploying to gh-pages from @ helemnmmm/helemnmmm.github.io@5bf234ac1da604f78de1d4090146328486194472 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,14 +9,7 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-blog",
-          title: "Blog",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/blog/";
-          },
-        },{id: "nav-codes",
+  },{id: "nav-codes",
           title: "Codes",
           description: "",
           section: "Navigation",
